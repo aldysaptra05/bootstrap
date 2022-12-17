feb30fe5250861f876fcbb09b53c43fdd17d1cff
aldysaptra05 <aldysaptra05@gmail.com>
2022-12-17 10:43:01 +0700

    menambahkan file file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// // event pada saat link di klik
+// $('.page-scroll').on('click', function(e){
+
+//     //ambil isi href
+//     var tujuan = $(this).attr('href');
+//     //tangkap elemen ybs
+//     var elemenTujuan = $(tujuan);
+    
+//     //pindahkan scroll
+//     console.log($('body').scrollTop());
+
+//     e.preventDefault();
+
+// });
