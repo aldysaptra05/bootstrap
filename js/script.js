0b51a6b79c573e7ba3e6cfd85c0894ae44ffce9c
aldysaptra05 <aldysaptra05@gmail.com>
2023-01-07 08:35:59 +0700

    Modifikasi parralax

--- js/script.js
+++ js/script.js
@@ -12,3 +12,42 @@
 //     e.preventDefault();
 
 // });
+
+
+
+// Parallax
+// about
+$(window).on('load', function() {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+})
+
+
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    //jumbotron
+
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    });
+
+    // Portfolio
+
+    if( wScroll > $('.portfolio').offset().top -250 ){
+        $('.portfolio .img-thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .img-thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i+1));
+        });
+    }
+    
+})
